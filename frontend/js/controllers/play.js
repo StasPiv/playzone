7d63919fbe059f948eb68e56f3d88dcacd28d3bb
Stanislav Pivovartsev <stanislav_pivovartsev@mail.ru>
2016-02-12 20:59:41 +0200

    feat(frontend): Add new chess board.

--- frontend/js/controllers/play.js
+++ frontend/js/controllers/play.js
@@ -4,14 +4,7 @@
 'use strict';
 
 playzoneControllers.controller('PlayCtrl', function ($scope) {
-    $scope.boardSettings = {
-        boardName: 'playzone',
-        pieceSet: 'leipzig',
-        pieceSize: '46',
-        pgnString: '1.e4 e5',
-        reverseFlip: false,
-        pauseBetweenMoves: '2000',
-        showCoordinates: true,
-        clickAndClick: true
+    $scope.boardConfig = {
+        position: 'start'
     };
 });

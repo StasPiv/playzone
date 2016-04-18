a2305a51e02f8b5d65615dee84565123e55c1fe5
Stanislav Pivovartsev <stanislav_pivovartsev@mail.ru>
2016-04-18 16:10:47 +0300

    hotfix

--- frontend/js/controllers/profile.js
+++ frontend/js/controllers/profile.js
@@ -6,10 +6,10 @@
 playzoneControllers.controller('ProfileCtrl', function ($scope, $rootScope, UserRest) {
     $scope.user_setting = {};
     
-    if ($rootScope.user && $rootScope.user.settings &&
-        $rootScope.user.settings['Piece type'] != 'undefined') {
-        $scope.user_setting['Piece type'] = $rootScope.user.settings['Piece type'].value;
+    while (!$rootScope.user || !$rootScope.user.settings) {
     }
+
+    $scope.user_setting['Piece type'] = $rootScope.user.settings['Piece type'].value;
 
     $scope.changeSetting = function (settingId, type, settingName) {
         var value = $scope.user_setting[settingName];

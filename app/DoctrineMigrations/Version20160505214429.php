740a599f61d92ccc5d0f1f047d0f35ee43e65381
Stanislav Pivovartsev <stanislav_pivovartsev@mail.ru>
2016-05-05 21:56:36 +0300

    fix(frontend): Fix chat in game room.

--- app/DoctrineMigrations/Version20160505214429.php
+++ app/DoctrineMigrations/Version20160505214429.php
@@ -18,6 +18,9 @@ class Version20160505214429 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql("UPDATE chat_message SET type = 1 WHERE type = 'common'");
+        $this->addSql("UPDATE chat_message SET type = 2 WHERE type = 'game'");
+
         $this->addSql('ALTER TABLE chat_message CHANGE type type INT NOT NULL');
         $this->addSql('CREATE INDEX type_idx ON chat_message (type)');
     }
@@ -32,5 +35,8 @@ class Version20160505214429 extends AbstractMigration
 
         $this->addSql('DROP INDEX type_idx ON chat_message');
         $this->addSql('ALTER TABLE chat_message CHANGE type type VARCHAR(255) DEFAULT NULL COLLATE utf8_unicode_ci');
+
+        $this->addSql("UPDATE chat_message SET type = 'common' WHERE type = 1");
+        $this->addSql("UPDATE chat_message SET type = 'game' WHERE type = 2");
     }
 }

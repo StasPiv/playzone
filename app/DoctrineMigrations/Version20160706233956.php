e8c1a9d9b549f115af52b89473ab830e6a3f9f43
Stanislav Pivovartsev <stanislav_pivovartsev@mail.ru>
2016-07-07 00:33:02 +0300

    fix(migration): Fix broken migration

--- app/DoctrineMigrations/Version20160706233956.php
+++ app/DoctrineMigrations/Version20160706233956.php
@@ -18,7 +18,6 @@ class Version20160706233956 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE solved_problem');
         $this->addSql('ALTER TABLE game ADD game_params LONGTEXT NOT NULL COMMENT \'(DC2Type:object)\'');
     }
 
@@ -30,7 +29,6 @@ class Version20160706233956 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE solved_problem (id INT AUTO_INCREMENT NOT NULL, fen LONGTEXT NOT NULL COLLATE utf8_unicode_ci, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE game DROP game_params');
     }
 }

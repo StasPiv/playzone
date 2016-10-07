e74c605923664729e18ee2922e9e753c9d954e35
Stanislav Pivovartsev <stanislav_pivovartsev@mail.ru>
2016-10-08 02:03:26 +0300

    Implement ping server service

---
+++ src/WebsocketClientBundle/Service/PingServerService.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stas
+ * Date: 08.10.16
+ * Time: 1:57
+ */
+
+namespace WebsocketClientBundle\Service;
+
+
+use CoreBundle\Model\Event\EventCommandInterface;
+use CoreBundle\Model\Event\EventInterface;
+use WebsocketClientBundle\Service\Client\PlayzoneClientSender;
+
+/**
+ * Class PingServerService
+ * @package WebsocketClientBundle\Service
+ */
+class PingServerService implements EventCommandInterface
+{
+    /** @var PlayzoneClientSender */
+    private $clientSender;
+
+    /**
+     * PingServerService constructor.
+     * @param PlayzoneClientSender $clientSender
+     */
+    public function __construct(PlayzoneClientSender $clientSender)
+    {
+        $this->clientSender = $clientSender;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function run()
+    {
+        $wsClient = new PlayzoneClient(
+            'ws://ws.playzone.immortalchess.net:8081/',
+            [
+                'timeout' => -1
+            ]
+        );
+
+        $this->clientSender
+             ->sendIntroductionFromRobot($wsClient);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setEventModel(EventInterface $eventModel)
+    {
+        // TODO: Implement setEventModel() method.
+    }
+
+}

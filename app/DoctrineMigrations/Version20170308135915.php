c19f73551dd73780b1fe93ac9d538a1e1f5e40ec
Stanislav Pivovartsev <stanislav_pivovartsev@mail.ru>
2017-03-08 13:59:59 +0200

    Add missing game_id field

--- app/DoctrineMigrations/Version20170308135915.php
+++ app/DoctrineMigrations/Version20170308135915.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20170308135455 extends AbstractMigration
+class Version20170308135915 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -18,7 +18,7 @@ class Version20170308135455 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE game_move CHANGE game_id game_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE game_move ADD game_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE game_move ADD CONSTRAINT FK_42B5B54E48FD905 FOREIGN KEY (game_id) REFERENCES game (id)');
         $this->addSql('CREATE INDEX IDX_42B5B54E48FD905 ON game_move (game_id)');
     }
@@ -33,6 +33,6 @@ class Version20170308135455 extends AbstractMigration
 
         $this->addSql('ALTER TABLE game_move DROP FOREIGN KEY FK_42B5B54E48FD905');
         $this->addSql('DROP INDEX IDX_42B5B54E48FD905 ON game_move');
-        $this->addSql('ALTER TABLE game_move CHANGE game_id game_id INT NOT NULL');
+        $this->addSql('ALTER TABLE game_move DROP game_id');
     }
 }
